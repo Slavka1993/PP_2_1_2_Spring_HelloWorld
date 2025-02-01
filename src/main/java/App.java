b66a8f9c300a5cb03c91dbd23396cb8ac08f15ed
slavk <slavka.cherenkov@gmail.com>
2025-02-01 17:11:26 +0300

    solution

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,16 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
+
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+
+        Cat cat1 = applicationContext.getBean("cat", Cat.class);
+        Cat cat2 = applicationContext.getBean("cat", Cat.class);
+        System.out.println("Ссылки на объекты bean и bean2: " + (bean==bean2));
+        System.out.println("Ссылки на объекты cat1 и cat2: " + (cat1==cat2));
     }
 }
